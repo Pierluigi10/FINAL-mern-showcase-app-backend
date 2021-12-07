1b74b0e0d8a3da84ff5fbe71d09cb80bda94c766
pierluigi <pierluigibaiano@gmail.com>
2021-12-07 09:25:35 +0100

    add collection to UserSchema

--- models/User.js
+++ models/User.js
@@ -1,18 +1,21 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-	firstName: {'type': String, "required": true},
-	lastName: {'type': String, "required": true},
-	username: {'type': String, "required": true},
-	email: {'type': String, "required": true},
-	accessGroups: {'type': String, "required": true},
-	password: { 'type': String, "required": true}
-}, 
-{
-	timestamps: true,
-	versionKey: false
-})
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    username: { type: String, required: true },
+    email: { type: String, required: true },
+    accessGroups: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+    collection: "users",
+  }
+);
 
-const UserModel = mongoose.model('User', UserSchema);
+const UserModel = mongoose.model("User", UserSchema);
 
 export default UserModel;
